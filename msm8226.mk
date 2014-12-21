e018657492cbbc0b62f85a4fd1f0ef842c98061b
Rox- <r.rozic97@gmail.com>
2014-12-22 00:36:03 +0100

    msm8226-common: Remove FM feature

--- msm8226.mk
+++ msm8226.mk
@@ -86,11 +86,11 @@ PRODUCT_PACKAGES += \
     libebtc
 
 # FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    FMRecord \
+#    libqcomfm_jni \
+#    qcom.fmradio
 
 # GPS
 PRODUCT_PACKAGES += \
