37ee2b3c8ab7156657eb8ca7ff031ce103049daf
Rox- <r.rozic97@gmail.com>
2015-01-09 17:17:16 +0100

    msm8226-common: Bring back custom lun path
    *Suport for custom lun path should be merged soon

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,9 +75,10 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Partitions
+# Partitions and Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8226
