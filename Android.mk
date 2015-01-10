a5356534a80fa2b36020eead6a17bd520950652d
Rox- <r.rozic97@gmail.com>
2015-01-10 14:04:36 +0100

    msm8226-common: Don't symlink keymaster firmware
    *Instead of symlinking keymaster firmware, copy it from vendor
    *This fixes keystore

--- Android.mk
+++ Android.mk
@@ -56,18 +56,6 @@ $(ISDB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(ISDB_SYMLINKS)
 
-KM_IMAGES := \
-    keymaster.b00 keymaster.b01 keymaster.b02 keymaster.b03 keymaster.mdt
-
-KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/keymaster/,$(notdir $(KM_IMAGES)))
-$(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Keymaster firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/keymaste$(suffix $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
-
 MBA_IMAGES := \
     mba.b00 mba.mdt
 
