5bc8b183ea42ec5cc98b769e13dcd471e9c90551
Nikola Majkić <n1kolaa924@gmail.com>
2016-02-21 23:31:52 +0100

    Revert "Revert Jmalloc"
    This reverts commit cad1ca97055a744bf6759264f2a4e63b268c3c98.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,9 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 # Kernel
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
+# Memory
+MALLOC_IMPL := dlmalloc
+
 # Partitions and Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USERIMAGES_USE_EXT4 := true
