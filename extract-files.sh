2433eaf4a542515d7f3261bc5862be7df041f9c8
Bruno Martins <bgcngm@gmail.com>
2017-08-23 14:43:23 +0100

    msm8226-common: Update extractors to use Lineage vendor
     * Match the branding change as well
    
    Change-Id: Ie39f8f52f3a7d552c82c0da643732af447aeb91e
    Signed-off-by: iKeramat <iKeramat80@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -22,12 +22,13 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 # Override anything that may come from the calling environment
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 BOARD=msm8226
 DEVICE_COMMON=${BOARD}-common
 VENDOR=samsung
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +52,7 @@ else
 fi
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
 
