522d3b30a999ef2fef896ab210acbffc23655165
Wei Wang <wvw@google.com>
2018-02-02 13:01:38 -0800

    msm8226-common: add SysUI and Settings to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666

--- msm8226.mk
+++ msm8226.mk
@@ -190,6 +190,11 @@ PRODUCT_PACKAGES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
+    
+# Preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
 
 # Ramdisk
 PRODUCT_PACKAGES += \
