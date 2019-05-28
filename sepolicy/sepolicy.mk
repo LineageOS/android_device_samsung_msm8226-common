a8db34282a1e3be1db466d923bb3e9dd8fdcf385
Anonymous <none@none.none>
2019-05-28 14:03:21 +0200

    msm8226-common: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I50ba8396529fe37fd8724a3ad4f5830f4244b9a3
    Signed-off-by: Felix <google@ix5.org>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -17,7 +17,7 @@
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/samsung/msm8226-common/sepolicy/common
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/samsung/msm8974-common/sepolicy/private
