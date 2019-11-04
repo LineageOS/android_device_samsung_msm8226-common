5d120f71c209fb46f9811e84d93ef9064e39b24a
Couchy <couchymail@gmail.com>
2019-11-04 15:25:52 +0000

    msm8226-common: camera: add libnativewindow and libgui libs
    These are now needed to access some native buffer headers.
    
    Change-Id: Ibc464415ef60109bbd66cd0b462e4197011545a3

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl libgui libhidltransport libsensor \
+    libhardware liblog libnativewindow libgui libcamera_client libutils libcutils libdl libgui libhidltransport libsensor \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
