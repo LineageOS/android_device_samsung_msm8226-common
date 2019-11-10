a0e92479cafe48159a1debf2fe065d08053e445c
pythonlimited <leonardomeitz@gmail.com>
2019-11-10 16:52:48 +0100

    msm8226-common: uprev qti power HAL
    Change-Id: Ic12c47a7c083f37be1128f57eee08c8e4a32c375

--- msm8226.mk
+++ msm8226.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
     
 # Preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
