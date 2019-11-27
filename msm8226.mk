29163785c2674c9c3b2b3b021c9a7c67a84f8ce8
Paul Keith <javelinanddart@gmail.com>
2019-11-27 15:39:09 -0800

    msm8226-common: Migrate all properties system.prop
    * We already set props via system.prop, so let's not mix
      and match between that and PRODUCT_PROPERTY_OVERRIDES.
    
    Change-Id: I92c6863b8393640e2239decd7f089ee890b3c7f7

--- msm8226.mk
+++ msm8226.mk
@@ -40,56 +40,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# System properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=false \
-    av.offload.enable=true\
-    ro.af.client_heap_size_kbyte=7168 \
-    ro.audio.flinger_standbytime_ms=300
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196608
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.graphics.vulkan.disable=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rild.nitz_plmn="" \
-    persist.rild.nitz_long_ons_0="" \
-    persist.rild.nitz_long_ons_1="" \
-    persist.rild.nitz_long_ons_2="" \
-    persist.rild.nitz_long_ons_3="" \
-    persist.rild.nitz_short_ons_0="" \
-    persist.rild.nitz_short_ons_1="" \
-    persist.rild.nitz_short_ons_2="" \
-    persist.rild.nitz_short_ons_3="" \
-    ril.subscription.types=NV,RUIM
-    
-PRODUCT_PROPERTY_OVERRIDES += \
-    pm.dexopt.shared=quicken    
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m    
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.boot-dex2oat-threads=4 \
-    dalvik.vm.dex2oat-swap=false \
-    dalvik.vm.checkjni=false \
-    dalvik.vm.dex2oat-flags=--no-watch-dog \
-    dalvik.vm.dex2oat-threads=4 \
-    dalvik.vm.image-dex2oat-threads=4 \
-    dalvik.vm.verify-bytecode=false \
-    persist.sys.purgeable_assets=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
