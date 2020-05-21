1e1d68d143c1901e8aa45a80910e5fd8e33249b7
Felix <google@ix5.org>
2020-05-22 00:45:29 +0200

    msm8226-common: BoardConfig: Use SYSTEM_*_SEPOLICY_DIRS
    The old variable name is being deprecated on API level 31+.
    
    See https://android-review.googlesource.com/q/topic:"board-system-ext-sepolicy"
    
    Signed-off-by: Felix <google@ix5.org>
    
    fcuzzocrea : also fix private sepolicy path missed during 8974 -> 8226
                 conversion
    
    Change-Id: I9b65f43060d503ab2d05ced8742b3dcce699c8f5

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -19,5 +19,5 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 # Board specific SELinux policy variable definitions
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/samsung/msm8226-common/sepolicy/common
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/samsung/msm8974-common/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/samsung/msm8226-common/sepolicy/private
