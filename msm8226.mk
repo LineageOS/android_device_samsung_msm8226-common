741e1a0599fe2c8e609b81ee2560273d35f7c0b0
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-09-25 14:34:56 +0200

    msm8226: build keystore hal
    Change-Id: Idf14e32e68f6d889ebef91e00fe0057f95fd242d

--- msm8226.mk
+++ msm8226.mk
@@ -123,7 +123,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    keystore.msm8226
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
