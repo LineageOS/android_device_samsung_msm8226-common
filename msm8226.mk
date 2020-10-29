72dfa30720921119442f189c24fef2c56022e0f7
Arne Coucheron <arco68@gmail.com>
2020-10-29 09:28:12 +0100

    msm8226-common: Move android.hardware.audio to hwbinder
    Change-Id: I63be890030e9d2d3c5f2ca35ecd8142e6fa92d46

--- msm8226.mk
+++ msm8226.mk
@@ -47,6 +47,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio_policy.msm8226 \
     audio.a2dp.default \
