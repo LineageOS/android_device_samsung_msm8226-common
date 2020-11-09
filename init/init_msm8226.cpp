b4cb4021650dc64cf42d89106e557155743882aa
Arne Coucheron <arco68@gmail.com>
2020-11-09 03:27:32 +0100

    msm8226-common: init: Override system_ext props too
    Change-Id: I4114ab9547dbfea4ba2d043a6be55a9694ce9de3

--- init/init_msm8226.cpp
+++ init/init_msm8226.cpp
@@ -46,6 +46,7 @@ std::vector<std::string> ro_product_props_default_source_order = {
     "odm.",
     "vendor.",
     "system.",
+    "system_ext.",
 };
 
 void property_override(char const prop[], char const value[], bool add)
