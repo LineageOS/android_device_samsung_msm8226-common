8f600043de838d3996c3b2fd2afa4f57aaf3ba45
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-11-18 17:40:40 +0100

    msm8226-common: stop building qcom touch service in common tree
     * makes no sense to build it for every devices when only some might
       support advanced touch features
    
    Change-Id: I49a0183bd66b64eb69833e018241fd6bbad7ffe4

--- msm8226.mk
+++ msm8226.mk
@@ -192,10 +192,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Touch features
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
