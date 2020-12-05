c6521b1df740f806b53387b7220ff9936722a4be
Bruno Martins <bgcngm@gmail.com>
2020-12-05 12:50:11 +0000

    msm8226-common: Kill legacy keystore
    Legacy credential hash support was obsolete and got removed completely.
    From now on all the devices need to rely on gatekeeper, which
    in case of legacy devices should be the software implementation.
    
    This fixes setting a lockscreen PIN/password/pattern.
    
    Ref: https://android.googlesource.com/platform/frameworks/base/+/a758727c963948ae07e97c977a63b882216e8971
    
    Change-Id: I020d1807db13b73f9990ac3704eadba49b65ee5c

--- msm8226.mk
+++ msm8226.mk
@@ -128,8 +128,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    keystore.msm8226
+    android.hardware.keymaster@3.0-service
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
