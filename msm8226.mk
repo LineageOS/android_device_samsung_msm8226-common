24f8657ddded5ff06cc3e58766a989a92bfd9b65
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    msm8226-common: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

--- msm8226.mk
+++ msm8226.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2012 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -115,7 +115,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
-    
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
-    
+
 # Preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
@@ -241,7 +241,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prima/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/samsung/msm8226-common/msm8226-common-vendor.mk)
+$(call inherit-product, vendor/samsung/msm8226-common/msm8226-common-vendor.mk)
 
 # common msm8226
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
