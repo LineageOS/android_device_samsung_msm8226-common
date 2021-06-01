e8559343cc6922cbf48bbec64c64467e924e6daa
matteo0026 <marmotta200026@gmail.com>
2021-06-01 22:53:02 +0200

    msm8226-common: add correct WCNSS_qcom_cfg.ini file destination
    * E wcnss_service: source file do not exist /vendor/etc/wifi/WCNSS_qcom_cfg.ini
    * If I remove that file from /system/etc/firmware/wlan/prima, wifi stops
    working, so I copy it in both directories.
    
    Change-Id: Ie9712adb8e0869434c7c956677697c84046f64fc

--- msm8226.mk
+++ msm8226.mk
@@ -234,6 +234,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/hostapd_default.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/hostapd/hostapd_default.conf \
     $(LOCAL_PATH)/prima/WCNSS_cfg.dat:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/prima/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/prima/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/prima/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
