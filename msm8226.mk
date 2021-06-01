7a0eb81dfd803ceaec35be055f4e52b71f4493ad
matteo0026 <marmotta200026@gmail.com>
2021-06-01 21:22:56 +0200

    msm8226-common: just define low-end video media codecs
    By looking into logs, standard file isn't even used.
    
    Change-Id: Ia91e5d7e7e0db1b1f438a603329333dd24f928f6

--- msm8226.mk
+++ msm8226.mk
@@ -133,7 +133,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml  \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     device/samsung/msm8226-common/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
